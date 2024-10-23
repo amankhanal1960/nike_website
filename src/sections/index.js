86ae6d9eb09cd177157a08481ec93653d12b4466
Aman Khanal <amankhanal1960@gmail.com>
2024-10-23 12:50:05 +0545

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  Footer,
+  Subscribe,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+};
