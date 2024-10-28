02e0a2500ead29567a66eefc3ddd7d3ab2c08472
Aman Khanal <amankhanal1960@gmail.com>
2024-10-28 22:17:51 +0545

    update

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { products } from "../constants";
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section
+      id="products"
+      className="max-container
+    max-sm:mt-12"
+    >
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our Popular Products
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Exercitationem expedita deserunt dolorum impedit cum. Amet adipisci
+          maiores nisi obcaecati sapiente voluptatem dolor maxime voluptate
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
